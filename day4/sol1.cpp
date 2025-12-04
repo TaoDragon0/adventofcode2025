b59a1e1170c58064ecb137c6483507514a4ec81b
TaoDragon0 <136400332+TaoDragon0@users.noreply.github.com.>
2025-12-04 11:35:13 -0500

    day4 sols

---
+++ day4/sol1.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+  
+  int rows;
+  int columns;
+  cin >> rows >> columns;
+  int res = 0;
+
+  int when_the[rows+2][columns+2];
+  for (int i = 0; i < rows+2; i++) {
+    when_the[i][0] = 0;
+    when_the[i][columns+1] = 0;
+  }
+  for (int i = 0; i < columns+2; i++) {
+    when_the[0][i] = 0;
+    when_the[rows+1][i] = 0;
+  }
+
+  for (int row = 0; row < rows; row++) {
+    string str_row;
+    cin >> str_row;
+    for (int column = 0; column < columns; column++) {
+      if (str_row.at(column) == '@') {
+        when_the[row+1][column+1] = 1;
+      } else {
+        when_the[row+1][column+1] = 0;
+      }
+    }
+  }
+
+  for (int row = 0; row < rows; row++) {
+    for (int column = 0; column < columns; column++) {
+      if (when_the[row+1][column+1] == 1) {
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 3; j++) {
+            sum += when_the[row+i][column+j];
+          }
+        }
+        if (sum < 5) {
+          res++;
+        }
+      } 
+    }
+  }
+
+  cout << res;
+
+  return 0;
+}
